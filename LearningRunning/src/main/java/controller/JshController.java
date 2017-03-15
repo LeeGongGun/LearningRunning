f69ae23d9d429d6d055b7a8c69fcd95ff8d795d1
DS <DS@603-07>
2017-03-15 12:39:08 +0900

    오후 12:36 2017-03-15 JSH

--- LearningRunning/src/main/java/controller/JshController.java
+++ LearningRunning/src/main/java/controller/JshController.java
@@ -1,16 +1,21 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartResolver;
 
 import dao.JshDao;
-import dao.LggDao;
 @Controller
 public class JshController {
 	private JshDao jshDao;
 	private MultipartResolver multipartResolver;
 	public void setJshDao(JshDao jshDao) {
 		this.jshDao = jshDao;
+	}
+	
+	@RequestMapping(value="attendance/attendancePerson", method=Requestmethod.GET)
+	public String attendIndividualGet {
+		
 	}
 
 	
