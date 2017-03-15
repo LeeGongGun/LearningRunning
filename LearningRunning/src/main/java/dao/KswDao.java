e19d4702ec326ab281ec4968df09215767342a01
DS <DS@603-08>
2017-03-15 12:35:08 +0900

    test

--- LearningRunning/src/main/java/dao/KswDao.java
+++ LearningRunning/src/main/java/dao/KswDao.java
@@ -1,9 +1,14 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 
 
@@ -14,4 +19,17 @@ public class KswDao{
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
+	
+	public int testConn() {
+		
+		List<Integer> result = jdbcTemplate.query("SELECT count(*) FROM member ",
+				new RowMapper<Integer>(){
+					@Override
+					public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Integer cnt = rs.getInt(1);
+						return cnt;
+					}
+				});
+		return result.isEmpty()?0:result.get(0);
+	}	
 }
