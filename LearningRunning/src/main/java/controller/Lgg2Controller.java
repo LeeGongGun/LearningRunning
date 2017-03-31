126d7237d2af69be4d63e2e86f7cc0bbed2572ea
ds <ds@ds601-12>
2017-03-31 17:32:01 +0900

    오후 5:31 2017-03-31 lgg

--- LearningRunning/src/main/java/controller/Lgg2Controller.java
+++ LearningRunning/src/main/java/controller/Lgg2Controller.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.Cookie;
@@ -11,14 +14,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartResolver;
 
 import bean.AuthInfo;
 import bean.AuthMember;
+import bean.Classes;
+import bean.PersonSubList;
+import command.AttendancePersonCommand;
 import command.LoginCommand;
 import command.LoginCommandValidator;
+import command.PersonSearch;
 import dao.LggDao;
 import exception.IdPasswordNotMatchException;
 
@@ -76,5 +85,204 @@ public class Lgg2Controller {
 		return "redirect:/login";
 	}
 	
+	@RequestMapping(value = "/attendance/person", method = RequestMethod.GET)
+	public String attendPersonSubListGet(Model model) {
+		int id = 1;
+		List<Classes> personSubList = lggDao.classList(id);
+		String stuSubject = lggDao.getSubjectName(id);
+		double attendRate = lggDao.getAttendRate(id);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("subject", stuSubject);
+		model.addAttribute("personSubList", personSubList);
+		return "attendance/attendancePersonSubList";
+	}
+
+	@RequestMapping(value = "/attendance/person/{id}", method = RequestMethod.GET)
+	public String attendPersonGet(@PathVariable("id") int studentId, Model model, PersonSearch personsearch) {
+		
+		List<PersonSubList> personSubList =lggDao.selectSubPerson(studentId);
+		model.addAttribute("personSubList", personSubList);
+		
+		String student = lggDao.getStudentName(studentId);
+		String studentEmail = lggDao.getStudentEmail(studentId);
+		double attendRate = lggDao.getAttendRate(studentId);
+		List<AttendancePersonCommand> attendancePersonCommand = lggDao.selectAllPerson(studentId);
+		String stuSubject = lggDao.getSubjectName(studentId);
+		
+		int countAll = lggDao.count(studentId);
+		int attendAll = lggDao.attendCount(studentId);
+		int late = lggDao.lateCount(studentId);
+		int goOut = lggDao.goOutCount(studentId);
+		int absentCount = lggDao.absentCount(studentId);
+		int leaveEarlyCount = lggDao.leaveEarlyCount(studentId);
+
+		model.addAttribute("countAll", countAll);
+		model.addAttribute("attendAll", attendAll);
+		model.addAttribute("late", late);
+		model.addAttribute("goOut", goOut);
+		model.addAttribute("absentCount", absentCount);
+		model.addAttribute("leaveEarlyCount", leaveEarlyCount);
+
+		model.addAttribute("PersonSearch", personsearch);
+		model.addAttribute("student", student);
+		model.addAttribute("studentEmail", studentEmail);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("stuSubject", stuSubject);
+		model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+		return "attendance/attendancePerson";
+	}
+
+	@RequestMapping(value = "/attendance/person/{id}", method = RequestMethod.POST)
+	public String attendPersonPost(@PathVariable("id") int studentId,
+			@RequestParam(value = "from", required = false) String strFrom,
+			@RequestParam(value = "to", required = false) String strTo, Model model) {
+
+		String student = lggDao.getStudentName(studentId);
+		String studentEmail = lggDao.getStudentEmail(studentId);
+		double attendRate = lggDao.getAttendRate(studentId);
+
+		int fromForCondition = Integer.parseInt(strFrom);
+		int toForCondition = Integer.parseInt(strTo);
+		
+		List<AttendancePersonCommand> attendancePersonCommand = 
+				lggDao.searchPersonPeriod(studentId, strFrom, strTo);
+		model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+		
+//		if (fromForCondition < toForCondition) {
+//			List<AttendancePersonCommand> attendancePersonCommand = 
+//					jshDao.searchPersonPeriod(studentId, strFrom, strTo);
+//			model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+//			
+//		} else {
+//			writer.println("<script type='text/javascript'>");
+//			writer.println("alert('�߸��� �Է��Դϴ�.');");
+//			writer.println("history.back();");
+//			writer.println("</script>");
+//			writer.flush();
+//			List<AttendancePersonCommand> attendancePersonCommand = jshDao.selectAllPerson(studentId);
+//			model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+//
+//		}
+		
+		
+		String stuSubject = lggDao.getSubjectName(studentId);
+
+		int countAll = lggDao.count(studentId);
+		int attendAll = lggDao.attendCount(studentId);
+		int late = lggDao.lateCount(studentId);
+		int goOut = lggDao.goOutCount(studentId);
+		int absentCount = lggDao.absentCount(studentId);
+		int leaveEarlyCount = lggDao.leaveEarlyCount(studentId);
+
+		model.addAttribute("countAll", countAll);
+		model.addAttribute("attendAll", attendAll);
+		model.addAttribute("late", late);
+		model.addAttribute("goOut", goOut);
+		model.addAttribute("absentCount", absentCount);
+		model.addAttribute("leaveEarlyCount", leaveEarlyCount);
+
+		model.addAttribute("from", strFrom);
+		model.addAttribute("to", strTo);
+		model.addAttribute("student", student);
+		model.addAttribute("studentEmail", studentEmail);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("stuSubject", stuSubject);
+		return "attendance/attendancePerson";
+	}
+
+	public Date transDate(String d) {
+		Date date = null;
+		if (d != null) {
+			SimpleDateFormat transFormat;
+			transFormat = new SimpleDateFormat("yyMMdd");
+			try {
+				date = transFormat.parse(d);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return date;
+	}
+
+	// ����
+	@RequestMapping(value = "/attendance/beLate/{id}", method = RequestMethod.GET)
+	public String belateGet(@PathVariable("id") int studentId, Model model, PersonSearch personsearch) {
+		String student = lggDao.getStudentName(studentId);
+		String studentEmail = lggDao.getStudentEmail(studentId);
+		String stuSubject = lggDao.getSubjectName(studentId);
+		double attendRate = lggDao.getAttendRate(studentId);
+		List<AttendancePersonCommand> attendancePersonCommand = lggDao.beLate(studentId);
+		model.addAttribute("PersonSearch", personsearch);
+		model.addAttribute("student", student);
+		model.addAttribute("studentEmail", studentEmail);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("stuSubject", stuSubject);
+		model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+		return "attendance/attendancePerson";
+	}
+
+	// ����
+	@RequestMapping(value = "/attendance/goOut/{id}", method = RequestMethod.GET)
+	public String goOutGet(@PathVariable("id") int studentId, Model model,
+			@RequestParam(value = "from", required = false) String strFrom,
+			@RequestParam(value = "to", required = false) String strTo) {
+
+		String student = lggDao.getStudentName(studentId);
+		String studentEmail = lggDao.getStudentEmail(studentId);
+		String stuSubject = lggDao.getSubjectName(studentId);
+		double attendRate = lggDao.getAttendRate(studentId);
+		List<AttendancePersonCommand> attendancePersonCommand = lggDao.goOut(studentId);
+
+		model.addAttribute("from", strFrom);
+		model.addAttribute("to", strTo);
+		model.addAttribute("student", student);
+		model.addAttribute("studentEmail", studentEmail);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("stuSubject", stuSubject);
+		model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+		return "attendance/attendancePerson";
+	}
+
+	// ����
+	@RequestMapping(value = "/attendance/leaveEarly/{id}", method = RequestMethod.GET)
+	public String attendPersonGet(@PathVariable("id") int studentId, Model model, PersonSearch personsearch,
+			@RequestParam(value = "from", required = false) String strFrom,
+			@RequestParam(value = "to", required = false) String strTo) {
+
+		String student = lggDao.getStudentName(studentId);
+		String studentEmail = lggDao.getStudentEmail(studentId);
+		String stuSubject = lggDao.getSubjectName(studentId);
+		double attendRate = lggDao.getAttendRate(studentId);
+		List<AttendancePersonCommand> attendancePersonCommand = lggDao.leaveEarly(studentId);
+		model.addAttribute("from", strFrom);
+		model.addAttribute("to", strTo);
+		model.addAttribute("PersonSearch", personsearch);
+		model.addAttribute("student", student);
+		model.addAttribute("studentEmail", studentEmail);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("stuSubject", stuSubject);
+		model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+		return "attendance/attendancePerson";
+	}
+
+	// �Ἦ
+	@RequestMapping(value = "/attendance/absent/{id}", method = RequestMethod.GET)
+	public String absentGet(@PathVariable("id") int studentId, Model model, PersonSearch personsearch) {
+		String memberId = lggDao.getMemberId(studentId);
+		String student = lggDao.getStudentName(studentId);
+		String studentEmail = lggDao.getStudentEmail(studentId);
+		String stuSubject = lggDao.getSubjectName(studentId);
+		double attendRate = lggDao.getAttendRate(studentId);
+		List<AttendancePersonCommand> attendancePersonCommand = lggDao.absent(studentId);
+
+		model.addAttribute("memberId", memberId);
+		model.addAttribute("PersonSearch", personsearch);
+		model.addAttribute("student", student);
+		model.addAttribute("studentEmail", studentEmail);
+		model.addAttribute("attendRate", attendRate);
+		model.addAttribute("stuSubject", stuSubject);
+		model.addAttribute("attendancePersonCommand", attendancePersonCommand);
+		return "attendance/attendancePerson";
+	}
 	
 }
--- LearningRunning/src/main/java/dao/LggDao.java
+++ LearningRunning/src/main/java/dao/LggDao.java
@@ -15,8 +15,10 @@ import bean.Attendance;
 import bean.AuthMember;
 import bean.ClassJoinMem;
 import bean.Classes;
+import bean.PersonSubList;
 import bean.TempAttendance;
 import command.AttendanceInsertCommand;
+import command.AttendancePersonCommand;
 import command.MemberSearchCommand;
 import command.ClassesSearchCommand;
 
@@ -117,7 +119,16 @@ public class LggDao{
 		String sql = "update TEMP_ATTENDANCE set " + cName + " = ? where CLASS_ID = ? and M_ID IN ("+inSql+") and "+cName+" IS NULL ";
 		return jdbcTemplate.update(sql, command.getTime(),class_id);
 	}
+	public List<Classes> classList(int m_id) {
+		String sql = "select * "
+				+ "from  MEMBER_CLASS where M_ID=? "
+				+ "natural join CLASSES ";
+		List<Classes> result = jdbcTemplate.query(sql,classRowMapper,m_id);
+		return result;
 
+	}
+	
+	
 	public List<Classes> classList(ClassesSearchCommand command) {
 		String whereSql ="";
 		int tmp=0;
@@ -414,5 +425,204 @@ public class LggDao{
 	}
 
 
+	
+	
+	//jsh
+	
+	private RowMapper<AttendancePersonCommand> attendPersonRowMapper = new RowMapper<AttendancePersonCommand>() {
+		@Override
+		public AttendancePersonCommand mapRow(ResultSet rs, int rowNum) 
+				throws SQLException {
+			AttendancePersonCommand attendancePersonCommand = new AttendancePersonCommand(
+					rs.getInt("m_id"), 
+					rs.getInt("subject_id"),
+					rs.getInt("attend_id"),
+					rs.getDate("start_time"),	
+					rs.getDate("end_time"),	
+					rs.getDate("stop_time"),	
+					rs.getDate("restart_time"),	
+					rs.getString("attend_status"),	
+					rs.getString("subject_name"),
+					rs.getDate("subject_start"), 
+					rs.getDate("subject_end"),
+					rs.getString("subject_state"),
+					rs.getString("subject_comment"), 
+					rs.getString("m_email"),
+					rs.getString("m_name"),
+					rs.getInt("m_pass"),
+					rs.getString("m_app_u_no"));
+			return attendancePersonCommand;
+		}
+	};	
+	
+	private RowMapper<PersonSubList> personSubListRowMapper = new RowMapper<PersonSubList>() {
+		@Override
+		public PersonSubList mapRow(ResultSet rs, int rowNum) 
+				throws SQLException {
+			PersonSubList personSubList = new PersonSubList(
+					rs.getInt("m_id"),
+					rs.getInt("subject_id"),
+					rs.getString("subject_name"),
+					rs.getString("subject_state"),
+					rs.getString("m_email"),
+					rs.getString("m_name")
+					);
+			return personSubList;
+		}
+	};	
+	
+	//수업 일수 세기
+	public int count(int studentId ) {
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from attendance where m_id = ? ", Integer.class, studentId);
+		return count;
+	}
+	
+	//총 출석
+	public int attendCount(int studentId ) {
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from (select * from attendance where attend_status = '출석') "
+				+ "where m_id = ? ", Integer.class, studentId);
+		return count;
+	}
+		
+	//지각
+	public int lateCount(int studentId ) {
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from (select * from attendance where attend_status = '지각') "
+				+ "where m_id = ? ", Integer.class, studentId);
+		System.out.println(count);
+		return count;
+	}
+	
+	//외출
+	public int goOutCount(int studentId ) {
+		Integer count = jdbcTemplate.queryForObject(
+			"select count(*) from (select * from attendance where attend_status = '외출') "
+			+ "where m_id = ? ", Integer.class, studentId);
+		return count;
+	}
+	
+	//결석
+	public int absentCount(int studentId ) {
+		Integer count = jdbcTemplate.queryForObject(
+			"select count(*) from (select * from attendance where attend_status = '결석') "
+			+ "where m_id = ? ", Integer.class, studentId);
+		return count;
+	}
+	
+	//조퇴
+	public int leaveEarlyCount(int studentId ) {
+		Integer count = jdbcTemplate.queryForObject(
+			"select count(*) from (select * from attendance where attend_status = '조퇴') "
+			+ "where m_id = ? ", Integer.class, studentId);
+		return count;
+	}
+	
+	//attendancepersonSubList 페이지용 Dao
+	public  List<PersonSubList> selectSubPerson(int studentId) {
+		String sql = "select distinct m_id, subject_id, subject_name, subject_state, m_email, m_name from "
+				+ "attendance natural join subjects natural join member where m_id = ? ";
+		List<PersonSubList> results = jdbcTemplate.query(sql, personSubListRowMapper, studentId);
+		return results;
+	}
+	
+	public List<AttendancePersonCommand> selectAllPerson(int studentId) {
+		String sql = "select * from attendance natural join subjects natural join member where m_id = ? ";
+		List<AttendancePersonCommand> results = jdbcTemplate.query(sql, attendPersonRowMapper, studentId);
+		return results;
+	}
+
+	public String getStudentName(int studentId) {
+		String result = jdbcTemplate.queryForObject(
+				"select m_name from member where m_id = ? ", String.class, studentId);
+		return result;
+	}
+	
+	public String getStudentEmail(int studentId) {
+		String result = jdbcTemplate.queryForObject(
+				"select m_email from member where m_id = ? ", String.class, studentId);
+		return result;
+	}
+	
+	public String getSubjectName(int studentId) {
+		String result = jdbcTemplate.queryForObject(
+				"select distinct subject_name from attendance natural join subjects where m_id = ? ", 
+				String.class, studentId);
+		return result;
+	}
+	
+	public String getMemberId(int studentId) {
+		String result = jdbcTemplate.queryForObject(
+				"select distinct m_id from attendance natural join subjects where m_id = ? ", 
+				String.class, studentId);
+		return result;
+	}
+	
+	public double getAttendRate(int studentId){
+		Integer studentAttend = jdbcTemplate.queryForObject(
+				"select count(*) from (select * from attendance where m_id = ? and "
+				+ "start_time between to_date((select  trunc(sysdate,'MM') from dual), 'YY/MM/DD') and "
+				+ "to_date((select last_day(sysdate) from dual), 'YY/MM/DD')) "
+				+ "where attend_status in ('출석','외출','지각','조퇴') ",
+				Integer.class, studentId);
+		
+		Integer mustAttend = jdbcTemplate.queryForObject("select count(*) from "
+				+ "(select * from attendance where m_id = ?) where start_time "
+				+ "between to_date((select  trunc(sysdate,'MM') from dual), 'YY/MM/DD') and "
+				+ "to_date((select last_day(sysdate) from dual), 'YY/MM/DD') ",
+				Integer.class, studentId);
+		
+		double result = ((double) studentAttend / (double) mustAttend) * 100;
+		return result;
+	}
+	
+	
+	//기간별 조회
+	public List<AttendancePersonCommand> searchPersonPeriod (int studentId, String strFrom, String strTo) {
+		String sql = "select * from attendance natural join subjects natural join member where m_id = ? and"
+				+ " start_time between to_date(?, 'YY/MM/DD') and to_date(?, 'YY/MM/DD')";
+		List<AttendancePersonCommand> results = 
+				jdbcTemplate.query(sql, attendPersonRowMapper, studentId, strFrom, strTo);
+		return results;
+	} 
+	
+	//결석, 외출, 지각, 조퇴 조회
+	//결석
+	public List<AttendancePersonCommand> absent(int studentId){
+		String sql = "select * from (select * from attendance natural join subjects natural join member"
+				+ " where attend_status = '결석' ) where m_id = ? ";
+		List<AttendancePersonCommand> results= jdbcTemplate.query(sql, attendPersonRowMapper, studentId);
+		return results;
+	}
+	
+	//외출
+	public List<AttendancePersonCommand> goOut(int studentId){
+		String sql = "select * from (select * from attendance natural join subjects natural join member"
+				+ " where attend_status = '외출' ) where m_id = ? ";
+		List<AttendancePersonCommand> results= jdbcTemplate.query(sql, attendPersonRowMapper, studentId);
+		return results;
+	}
+	
+	//지각
+	public List<AttendancePersonCommand> beLate(int studentId){
+		String sql = "select * from (select * from attendance natural join subjects natural join member "
+				+ "where attend_status = '지각' ) where m_id = ? ";
+		List<AttendancePersonCommand> results= jdbcTemplate.query(sql, attendPersonRowMapper, studentId);
+		return results;
+	}
+	
+	
+	//조퇴
+	public List<AttendancePersonCommand> leaveEarly(int studentId){
+		String sql = "select * from (select * from attendance natural join subjects natural join member"
+				+ " where attend_status = '조퇴' ) where m_id = ? ";
+		List<AttendancePersonCommand> results= jdbcTemplate.query(sql, attendPersonRowMapper, studentId);
+		return results;
+	}
+	
+	
+	
+	
 
 }
